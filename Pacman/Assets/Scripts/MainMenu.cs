55692d5498c334eae8600b8a0271ea0d05946f38
kr0man0va <67516645+kr0man0va@users.noreply.github.com>
2022-09-30 12:14:15 -0400

    Updated Scenes

--- Pacman/Assets/Scripts/MainMenu.cs
+++ Pacman/Assets/Scripts/MainMenu.cs
@@ -7,7 +7,8 @@ public class MainMenu : MonoBehaviour
 {
     
     public void PlayGame() {
-        SceneManager.LoadScene("Pacman");
+        SceneManager.LoadScene("GameMode");
+        //SceneManager.LoadScene("Pacman");
     }
 
     public void QuitGame() {
